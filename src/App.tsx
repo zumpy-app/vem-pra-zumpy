4cb203124955ec15a6f8bec4263edc8afdfb48bd
juaniwk3 <juaniwk3@gmail.com>
2022-05-13 15:25:49 -0300

    topics

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
 import React from "react";
+import tags from "./content/tags.json";
 import "./styles/global.scss";
 
 function App() {
-  return <div className="App">Empty</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="left">
+          {tags.topics.map((topic, index) => {
+            return (
+              <div key={index}>
+                <button>{topic.title}</button>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
